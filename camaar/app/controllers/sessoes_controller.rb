a555f3b32ed34b6958b72709983c5442bdf3f442
Matheus Chagas <137133323+metiosbr@users.noreply.github.com>
2025-07-06 21:17:44 -0300

    Update sessoes_controller.rb
    Lógica do login de usuário e admin

--- camaar/app/controllers/sessoes_controller.rb
+++ camaar/app/controllers/sessoes_controller.rb
@@ -1,13 +1,33 @@
 class SessoesController < ApplicationController
   skip_before_action :requerer_login, only: [:new, :create]
-  skip_before_action :requerer_usuario_ativo, only: [:new, :create]
+  #skip_before_action :requerer_usuario_ativo, only: [:new, :create]
 
   def new
+    # Apenas renderiza o formulário de login
   end
 
   def create
+    usuario = Usuario.find_by(email: params[:email])
+
+    if usuario && usuario.authenticate(params[:password])
+      session[:usuario_id] = usuario.id
+
+      if !usuario.esta_ativo?
+        redirect_to redefinir_senha_path
+
+      elsif usuario.admin?
+        redirect_to admin_path
+      else
+        redirect_to root_path
+      end
+    else
+      flash.now[:alert] = "Email ou senha inválidos!"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    session[:usuario_id] = nil
+    redirect_to login_path, notice: "Logout efetuado com sucesso!"
   end
 end

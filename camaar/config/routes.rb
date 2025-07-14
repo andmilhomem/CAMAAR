8b73ebceb22511129a2f0a810132a4b89f06d09e
André Godoi <andmilhomem@hotmail.com>
2025-07-13 21:22:36 -0300

    Atualiza o READ.ME com informações sobre a sprint-2

--- camaar/config/routes.rb
+++ camaar/config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # rota de exemplo para testar se cucumber está funcionando
-  get "/hello", to: "pages#hello"
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check

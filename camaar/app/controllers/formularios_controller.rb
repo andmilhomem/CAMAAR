5973eb82fd20212abcfa74e578c1bbb24a4dbf9a
Paulo Filho <paulomacieltorresfilho@gmail.com>
2025-07-18 18:14:29 -0300

    adiciona logica para adicionar formularios, estilizacao do new-formulario

--- camaar/app/controllers/formularios_controller.rb
+++ camaar/app/controllers/formularios_controller.rb
@@ -19,12 +19,51 @@ class FormulariosController < ApplicationController
   end
 
   def new
+    load_form_options
   end
 
   def create
+    turmas_selecionadas = params[:turma_ids]
+    template_id = params[:template_id]
+    if not template_id.present?
+      redirect_to new_formulario_path, alert: "Selecione um template" and return
+    end
+    if not turmas_selecionadas.present?
+      redirect_to new_formulario_path, alert: "Selecione ao menos uma turma" and return
+    end
+
+    formularios = []
+    turmas_selecionadas.each do |turma_id|
+      formularios << Formulario.create(turma_id: turma_id)
+    end
+
+    template = Template.find(template_id)
+
+    formularios.each do |formulario|
+      template.questaos.each do |questao|
+        q_form = questao.dup
+        q_form.template_id = nil
+        q_form.formulario_id = formulario.id
+        q_form.save
+        if q_form.tipo.eql? "Radio"
+          questao.opcaos.each do |opcao|
+            op_form = opcao.dup
+            op_form.questao_id = q_form.id
+            op_form.save
+          end
+        end
+      end
+    end
+    redirect_to formularios_path, notice: formularios
   end
 
   private
+
+  def load_form_options
+    @templates = Template.all
+    @template_options = @templates.map do |template| [template.nome, template.id] end
+    @turmas = Turma.all
+  end
 
   def formulario_params
     params.require(:formulario).permit(
